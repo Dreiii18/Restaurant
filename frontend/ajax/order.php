c83407a70c9b4551ae27b04153a0204e936b39b1
Dreiii18 <andreiartap@gmail.com>
2024-11-05 09:50:43 -0800

    update order html to order php

--- frontend/ajax/order.php
+++ frontend/ajax/order.php
@@ -4,11 +4,113 @@ require_once($config["path"] . "/backend/core.php");
 
 $core = new Core();
 
-$orderList = $_REQUEST['orders'];
+if (isset($_REQUEST['orders'])) {
+    $orderList = $_REQUEST['orders'];
+}
 
 // $core->generateOrder();
-$order = $core->addOrder($orderList);
+// $order = $core->addOrder($orderList);
+
+function displayMainItem($id) {
+    global $core;
+    [$menuName, $menuDescription, $menuPrice] = $core->getMenuItem($id);
+    echo "
+        <h2 data-menu-id='{$id}'>{$menuName}</h2>
+        <p>{$menuDescription}. {$menuPrice}</p>
+        <button class='add-to-cart'>Add to Cart</button>
+    ";
+}
+
+function displaySubItems($ids) {
+    global $core;
+    foreach($ids as $id) {
+        [$menuName, $menuDescription, $menuPrice] = $core->getMenuItem($id);
+        echo "
+            <dt data-menu-id='{$id}'>{$menuName}</dt>
+            <dd>{$menuDescription}. {$menuPrice}</dd>
+            <dd><button class='add-to-cart'>Add to Cart</button></dd>
+        ";
+    }
+}
 ?>
-<h1>
-    <?php echo $order ?>
-</h1>
+
+<div class="row" id="starters">
+    <div class="row">
+        <div class="col text-align-center">
+            <h2 class="section-title">Starters</h2>
+        </div>
+    </div>
+    <div class="col feature-dish">
+        <img src="frontend/images/truffleMacCheeseBalls.jpg" class="d-block w-100" alt="Deep fried truffle mac & cheese balls">
+        <?php displayMainItem(1); ?>
+    </div>
+    <div class="col">
+        <dl>
+            <?php displaySubItems([2,3,4])?>
+        </dl>
+    </div>
+</div>
+<div class="row" id="entrees">
+    <div class="row">
+        <div class="col text-align-center">
+            <h2 class="section-title">Entrees</h2>
+        </div>
+    </div>
+    <div class="col feature-dish">
+        <img src="frontend/images/tomahawkSteak.jpg" class="d-block w-100" alt="Tomahawk Steak">
+        <?php displayMainItem(5)?>
+    </div>
+    <div class="col">
+        <dl>
+            <?php displaySubItems([6, 7, 8, 9])?>
+        </dl>
+    </div>
+</div>
+<div class="row" id="sides">
+    <div class="row">
+        <div class="col text-align-center">
+            <h2 class="section-title">Sides</h2>
+        </div>
+    </div>
+    <div class="col feature-dish">
+        <img src="frontend/images/truffleFries.jpg" class="d-block w-100" alt="Truffle fries">
+        <?php displayMainItem(10)?>
+    </div>
+    <div class="col">
+        <dl>
+            <?php displaySubItems([11, 12, 13])?>
+        </dl>
+    </div>
+</div>
+<div class="row" id="desserts">
+    <div class="row">
+        <div class="col text-align-center">
+            <h2 class="section-title">Desserts</h2>
+        </div>
+    </div>
+    <div class="col feature-dish">
+        <img src="frontend/images/pecanPie.jpg" class="d-block w-100" alt="Pecan Pie">
+        <?php displayMainItem(14)?>
+    </div>
+    <div class="col">
+        <dl>
+            <?php displaySubItems([15, 16])?>
+        </dl>
+    </div>
+</div>
+<div class="row" id="beverages">
+    <div class="row">
+        <div class="col text-align-center">
+            <h2 class="section-title">Beverages</h2>
+        </div>
+    </div>
+    <div class="col feature-dish">
+        <img src="frontend/images/whiskeyFlight.jpg" class="d-block w-100" alt="Whiskey Flight">
+        <?php displayMainItem(17)?>
+    </div>
+    <div class="col">
+        <dl>
+            <?php displaySubItems([18, 19, 20, 21])?>
+        </dl>
+    </div>
+</div>
--- frontend/js/order.js
+++ frontend/js/order.js
@@ -1,25 +1,40 @@
+let cartItems = [];
 $(document).ready(function() {
     // wait for to card button before proceeding order
     var orders = []
+
+    displayMenu();
+
     $('#checkout').click(function () {
         // passed from frontend
+        orders = [
+            {
+                menuid: 1,
+                quantity: 2
+            },
+            {
+                menuid: 2,
+                quantity: 3
+            }
+        ]
+        addOrder(orders);   
     });
-    orders = [
-        {
-            menuid: 1,
-            quantity: 2
-        },
-        {
-            menuid: 2,
-            quantity: 3
-        }
-    ]
-    addOrder(orders);   
     
-    $(".badge").on("click", function() {
-        $("#cart").toggle();
-        $("#restaurant_background").toggle();
-    }) 
+});
+
+$(document).on('click', '.add-to-cart', function() {
+    const menuId = $(this).closest('[data-menu-id]').data('menu-id');
+    const existingItem = cartItems.find(item => item.menuId === menuId);
+
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cartItems.push({ menuId: menuId, quantity: 1 });
+    }
+
+    console.log(cartItems);
+
+    updateCartDisplay();
 });
 
 function addOrder(orders) {
@@ -35,8 +50,33 @@ function addOrder(orders) {
     });
 }
 
-function displayOrder() {
+function displayMenu() {
+    $.ajax({
+        url: "frontend/ajax/order.php",
+        success: function(data) {
+            $('#menu').html(data);
+        }
+    })
+}
 
+function updateCartDisplay() {
+    $('#items-ordered').empty(); 
+
+    cartItems.forEach(item => {
+        $('#items-ordered').append(`
+            <div class="menu-item">
+                <div class="ordered-item">
+                    <h3>Tomahawk Steak</h3>
+                    <p>$359.99</p>
+                </div>
+                <div class="item-qty">
+                    <button type="button" class="subtract">-</button>
+                    <input type="text" class="quantity" value="1">
+                    <button type="button" class="add">+</button>
+                </div>
+            </div>
+        `);
+    });
 }
 
 
