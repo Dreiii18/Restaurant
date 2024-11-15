39586a1e3f4abeeeecfe42a6518f65f7d82b3ed3
j987654321 <jasonthechu@gmail.com>
2024-11-14 16:53:04 -0800

    figured out how to add my items to the order form figured out the dropdown showing specific items for a specific category

--- frontend/js/inventory.js
+++ frontend/js/inventory.js
@@ -64,3 +64,170 @@ $(document).ready(function () {
     });
 
 });
+
+// function configureDropDownLists(catDropdown, item) {
+//     var items = ['Select A Category'];
+//     var meat = ['Porkchop', 'fillet mignon', 'Blue'];
+//     var dairy = ['milk', 'egg'];
+//     var produce = ['potato', 'onion', 'apple'];
+//     var spices = ['rosemary', 'basil'];
+  
+//     switch (catDropdown.value) {
+//       case 'Category':
+//         item.options.length = 0;
+//         for (i = 0; i < items.length; i++) {
+//           createOption(item, items[i], items[i]);
+//         }
+//         break;
+//       case 'Meat':
+//         item.options.length = 0;
+//         for (i = 0; i < meat.length; i++) {
+//           createOption(item, meat[i], meat[i]);
+//         }
+//         break;
+//       case 'Dairy':
+//         item.options.length = 0;
+//         for (i = 0; i < dairy.length; i++) {
+//           createOption(item, dairy[i], dairy[i]);
+//         }
+//         break;
+//       case 'Produce':
+//         item.options.length = 0;
+//         for (i = 0; i < produce.length; i++) {
+//           createOption(item, produce[i], produce[i]);
+//         }
+//         break;
+//       case 'Spices':
+//         item.options.length = 0;
+//         for (i = 0; i < spices.length; i++) {
+//           createOption(item, spices[i], spices[i]);
+//         }
+//         break;
+//       default:
+//         item.options.length = 0;
+//         break;
+//     }
+  
+//   }
+  
+//   function createOption(catDropdown, text, value) {
+//     var opt = document.createElement('option');
+//     opt.value = value;
+//     opt.text = text;
+//     catDropdown.options.add(opt);
+//   }
+
+//   function addRow() {
+//     const divEle = document.getElementById("inputFields");
+//     divEle.innerHTML += `
+// <div>
+//   <label for="catDropdown" class="label-category">Category:</label>
+//                 <select id="catDropdown" name="category" class="category" onchange="configureDropDownLists(this,document.getElementById('item'))">
+//                     <option value="Category">Category</option>
+//                     <option value="Meat">Meat</option>
+//                     <option value="Dairy">Dairy</option>
+//                     <option value="Produce">Produce</option>
+//                     <option value="Spices">Spices/Dry</option>
+//                 </select>
+
+//                 <label for="item">Item:</label>
+//                 <select id="item" name="item">
+                    
+//                 </select>
+                
+//                 <label for="quantity" class="label-quantity">Quantity:</label>
+//                 <input type="number" name="quantity" class="quantity" size="2">
+              
+               
+            
+//                 <label for="supDropdown" class="label-supplier">Supplier:</label>
+//                 <select id="supDropdown" name="supplier" class="supplier">
+//                     <option>Suppliers</option>
+//                     <option>Max's Meat Co</option>
+//                     <option>Dairyland</option>
+//                     <option>Joe's Farm</option>
+//                     <option>Sam's Spices</option>
+//                 </select>
+
+//                 <br><br>
+// </div>
+// `;
+//   }
+
+function configureDropDownLists(catDropdown, itemDropdownId) {
+  const item = document.getElementById(itemDropdownId);
+  const items = ['Select A Category'];
+  const meat = ['Porkchop', 'fillet mignon', 'Blue'];
+  const dairy = ['milk', 'egg'];
+  const produce = ['potato', 'onion', 'apple'];
+  const spices = ['rosemary', 'basil'];
+
+  item.options.length = 0; // Clear existing options
+
+  switch (catDropdown.value) {
+    case 'Category':
+      items.forEach(i => createOption(item, i, i));
+      break;
+    case 'Meat':
+      meat.forEach(i => createOption(item, i, i));
+      break;
+    case 'Dairy':
+      dairy.forEach(i => createOption(item, i, i));
+      break;
+    case 'Produce':
+      produce.forEach(i => createOption(item, i, i));
+      break;
+    case 'Spices':
+      spices.forEach(i => createOption(item, i, i));
+      break;
+    default:
+      break;
+  }
+}
+
+function createOption(dropdown, text, value) {
+  const opt = document.createElement('option');
+  opt.value = value;
+  opt.text = text;
+  dropdown.options.add(opt);
+}
+
+function addRow() {
+  const divEle = document.getElementById("inputFields");
+  const uniqueId = `row-${Date.now()}`;
+  const catDropdownId = `${uniqueId}-catDropdown`;
+  const itemDropdownId = `${uniqueId}-item`;
+
+  divEle.innerHTML += `
+      <div>
+          <label for="${catDropdownId}" class="label-category">Category:</label>
+          <select id="${catDropdownId}" name="category" class="category" 
+              onchange="configureDropDownLists(this, '${itemDropdownId}')">
+              <option value="Category">Category</option>
+              <option value="Meat">Meat</option>
+              <option value="Dairy">Dairy</option>
+              <option value="Produce">Produce</option>
+              <option value="Spices">Spices/Dry</option>
+          </select>
+
+          <label for="${itemDropdownId}">Item:</label>
+          <select id="${itemDropdownId}" name="item" class="item">
+              <!-- Options will be populated based on category selection -->
+          </select>
+              
+          <label for="quantity" class="label-quantity">Quantity:</label>
+          <input type="number" name="quantity" class="quantity" size="2">
+              
+          <label for="supDropdown" class="label-supplier">Supplier:</label>
+          <select id="supDropdown" name="supplier" class="supplier">
+              <option>Suppliers</option>
+              <option>Max's Meat Co</option>
+              <option>Dairyland</option>
+              <option>Joe's Farm</option>
+              <option>Sam's Spices</option>
+          </select>
+
+          <br><br>
+      </div>
+  `;
+}
