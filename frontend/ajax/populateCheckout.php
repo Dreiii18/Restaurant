80496ad83aad816cbd2472f05164057fc2756d1e
Dreiii18 <andreiartap@gmail.com>
2024-11-19 18:22:25 -0800

    - check if user role is customer then populate if employee then don't populate

--- frontend/ajax/populateCheckout.php
+++ frontend/ajax/populateCheckout.php
@@ -6,6 +6,19 @@ require_once($config["path"] . "/backend/core.php");
 $core = new Core();
 
 $isLoggedIn = isset($_SESSION['user']);
-$userData = $isLoggedIn ? $core->getTransactionDetails($_SESSION['user']['userid']) : [];
+// $userData = $isLoggedIn ? $core->getTransactionDetails($_SESSION['user']['userid']) : [];
+if ($isLoggedIn) {
+    $userId = $_SESSION['user']['userid'];
+    $userRole = $core->getTableColumns('role', 'user', "userid = '{$userId}'")[0]['role'];
+
+    if ($userRole !== 'Customer') {
+        $userData = [];
+    } else {
+        $userData = $core->getTransactionDetails($userId);
+    }
+
+} else {
+    $userData = [];
+}
 // print_r($userData);
 echo json_encode($userData);
